df17a6d651cbfd34047662f6b9061b486d69ab61
Frederique <frederique.retsema@conclusionxforce.nl>
2025-08-26 12:51:56 +0200

    Added alert rule tags and resource group tags

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ then
     echo "No environment variables set, run setenv.sh first"
 else 
 
-    #terraform plan -var dynatrace-api-token="${DYNATRACE_API_TOKEN}" -var dynatrace-api-url="${DYNATRACE_API_URL}" -out terraform.plan 
-    #terraform apply terraform.plan
+    terraform plan -var dynatrace-api-token="${DYNATRACE_API_TOKEN}" -var dynatrace-api-url="${DYNATRACE_API_URL}" -out terraform.plan 
+    terraform apply terraform.plan
 
     cd code 
     func azure functionapp publish azure-alerts-dynatrace-fa --python
